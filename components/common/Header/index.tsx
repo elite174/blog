da6e7d3c358cbd0c0e03cda4d1b289d2fc723edb
Vladislav <lipatov.xex@gmail.com>
2020-09-09 23:58:14 +0500

    chore: first commit

---
+++ components/common/Header/index.tsx
@@ -0,0 +1,12 @@
+import React, { FC } from 'react';
+import { css } from 'emotion';
+
+const headerStyles = css`
+
+`;
+
+const Header: FC = () => {
+    return <header>
+        
+    </header>;
+};
